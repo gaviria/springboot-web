1d703875e274ef8f229158de6fb341094a5a39bd
Francisco Gaviria Reyes <fjgaviria@i4digital.com>
2024-04-10 22:06:28 -0500

    Pasar datos a la Vista

--- src/main/java/com/fgaviria/spring/springbootweb/controllers/UserController.java
+++ src/main/java/com/fgaviria/spring/springbootweb/controllers/UserController.java
@@ -1,13 +1,17 @@
 package com.fgaviria.spring.springbootweb.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
     @GetMapping("/details")
-    public String details() {
-        return "details";
+    public String details(Model model) {
+        model.addAttribute("title", "Hello World!");
+        model.addAttribute("name", "Francisco");
+        model.addAttribute("lastname", "Gaviria");
+        return "details"; // con el mismo nombre de la vista
     }
 }
